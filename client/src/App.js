e927ce3c95cfa5af3cec1213425423885cbb7633
James Holding <james.holding90@gmail.com>
2020-08-10 14:30:05 -0400

    Navbar functionality complete.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,35 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/Homepage";
 import Search from "./pages/Search";
-import PastTrips from "./pages/PastTrips"
-import Navbar from "./components/Navbar/Navbar";
+import PastTrips from "./pages/PastTrips";
+import Header from "./components/Appbar/AppBar";
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      <Header />
       <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route path="/search" component={Search} />
-        <Route path="/pasttrips" component={PastTrips} />
+        <Route
+          exact
+          from="/"
+          render={(props) => <Homepage {...props} />}
+        />
+        <Route
+          exact
+          path="/Search"
+          render={(props) => <Search {...props} />}
+        />
+        <Route
+          exact
+          path="/PastTrips"
+          render={(props) => <PastTrips {...props} />}
+        />
       </Switch>
     </Router>
   );
