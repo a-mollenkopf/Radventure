7b94e2959383b66f043109cf9e73417f18d90d0c
James Holding <james.holding90@gmail.com>
2020-08-10 11:51:01 -0400

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,12 @@ import "./App.css";
 import Homepage from "./pages/Homepage";
 import Search from "./pages/Search";
 import PastTrips from "./pages/PastTrips"
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/search" component={Search} />
