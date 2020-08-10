1182481cd0d582519d6a38ed6cb5c8462c582448
Alexis Mollenkopf <alexismollenkopf@gmail.com>
2020-08-10 11:49:24 -0400

    added buttons to all pages as well as functionality

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const Search = () => {
     return (
-        <div>
+        <div className="container">
            <h1>This is my Search page</h1> 
+           <Link class="waves-effect waves-light btn" to="/">Back</Link>
         </div>
     );
 };
