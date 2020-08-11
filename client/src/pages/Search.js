14c4221fbfe79d36ae12e85de26f60a7b6e9e3d1
James Holding <james.holding90@gmail.com>
2020-08-11 11:04:02 -0400

    moved save search button to map.jsx file.

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,42 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Map from "../components/Map/Map";
-import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Fab from "@material-ui/core/Fab";
-import NavigationIcon from "@material-ui/icons/Navigation";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    flexWrap: "wrap",
-    justify: "center",
-    "& > *": {
-      margin: theme.spacing(2, "auto"),
-      width: theme.spacing(145),
-      height: theme.spacing(70, "auto"),
-    },
-  },
-}));
 
 const Search = () => {
-  const classes = useStyles();
   return (
-    <div className={classes.root}>
-      <Paper elevation={3}>
-        {" "}
-        <Map />{" "}
-        <Fab
-          variant="extended"
-          size="medium"
-          color="primary"
-          aria-label="add"
-          className={classes.margin}
-        >
-          <NavigationIcon />
-          Save Search
-        </Fab>
-      </Paper>
+    <div>
+      <Map />
     </div>
   );
 };
