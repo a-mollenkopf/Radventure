46f9362cb547ee0f2c55fe042142b04d726d79f1
Alexis Mollenkopf <alexismollenkopf@gmail.com>
2020-08-12 11:40:08 -0400

    fixed bugs from merge

--- client/src/components/ViewOneTrip/ViewOneTrip.jsx
+++ client/src/components/ViewOneTrip/ViewOneTrip.jsx
@@ -2,14 +2,13 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import API from "../../utils/API";
-import Card from "@material-ui/core/Card";
+import { Link } from "react-router-dom";
 
 const styles = {
   ButtonsStyle: {
     background: "#02361C",
     color: "white",
     justifyContent: "center",
-    marginTop: 400,
   },
   CardStyles: {
     maxWidth: 1500,
@@ -41,48 +40,48 @@ const ViewOneTrip = () => {
 
   return (
     <div>
-      <div key={oneTripState._id}>
-        <form>
-          <div className="container">
-            <h1 className="text-center welcome">Details of Your Trip !</h1>
+    <div key={oneTripState._id}>
+      <form>
+        <div className="container">
+          <h1 className="text-center welcome">Details of Your Trip !</h1>
 
-            <h3> Start City Information</h3>
-            <h4> Street: {oneTripState.startStreet}</h4>
-            <h4>City: {oneTripState.startCity}</h4>
-            <h4> State: {oneTripState.startState}</h4>
-            <h4> Postal Code: {oneTripState.startPostalCode}</h4>
-            <h5>-------------------------------</h5>
-            <h3> Destination City Information</h3>
-            <h4> Street: {oneTripState.destinationStreet}</h4>
-            <h4>City: {oneTripState.destinationCity}</h4>
-            <h4> State: {oneTripState.destinationState}</h4>
-            <h4> Postal Code: {oneTripState.destinationPostalCode}</h4>
-            <div>
-                <Button
-                  id={oneTripState._id}
-                  type="submit"
-                  onClick={() => handleDelete(oneTripState._id)}
-                  size="large"
-                  style={styles.ButtonsStyle}
-                  href="/PastTrips"
-                >
-                  Delete
-                </Button>
-              <Link to={`/PastTrips/${oneTripState._id}/edit`}>
-                <Button
-                  id={oneTripState._id}
-                  type="submit"
-                  size="large"
-                  style={styles.ViewTripButtonStyle}
-                >
-                  Update
-                </Button>
-              </Link>
-            </div>
-          </form>
-        </div>
+          <h3> Start City Information</h3>
+          <h4> Street: {oneTripState.startStreet}</h4>
+          <h4>City: {oneTripState.startCity}</h4>
+          <h4> State: {oneTripState.startState}</h4>
+          <h4> Postal Code: {oneTripState.startPostalCode}</h4>
+          <h5>-------------------------------</h5>
+          <h3> Destination City Information</h3>
+          <h4> Street: {oneTripState.destinationStreet}</h4>
+          <h4>City: {oneTripState.destinationCity}</h4>
+          <h4> State: {oneTripState.destinationState}</h4>
+          <h4> Postal Code: {oneTripState.destinationPostalCode}</h4>
+          <div>
+              <Button
+                id={oneTripState._id}
+                type="submit"
+                onClick={() => handleDelete(oneTripState._id)}
+                size="large"
+                style={styles.ButtonsStyle}
+                href="/PastTrips"
+              >
+                Delete
+              </Button>
+            <Link to={`/PastTrips/${oneTripState._id}/edit`}>
+              <Button
+                id={oneTripState._id}
+                type="submit"
+                size="large"
+                style={styles.ButtonsStyle}
+              >
+                Update
+              </Button>
+            </Link>
+          </div>
+          </div>
+        </form>
       </div>
-    </Card>
+    </div>
   );
 };
 
