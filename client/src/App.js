3bff56a1cb86d8ed29a8d33aa3d0f9d1215986b4
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2020-08-11 21:09:27 -0400

    view details of the trip created

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,29 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Search from "./pages/Search";
 import Header from "./components/Appbar/AppBar";
 import PastTrips from "./pages/PastTrips";
+import ViewOneTrip from "./components/ViewOneTrip/ViewOneTrip";
+
 import { MapProvider } from "./contexts/MapProvider";
-
-
 
 function App() {
   return (
     <MapProvider>
-
-    <Router>
-      <Header />
-      <Switch>
-        <Route
-          exact
-          path="/"
-          render={(props) => <Search {...props} />}
-        />
-        <Route
-          exact
-          path="/PastTrips"
-          render={(props) => <PastTrips {...props} />}
-        />
-      </Switch>
-    </Router>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" render={(props) => <Search {...props} />} />
+          <Route
+            exact
+            path="/PastTrips"
+            render={(props) => <PastTrips {...props} />}
+          />
+          <Route exact path="/PastTrips/:id" component={ViewOneTrip} />
+        </Switch>
+      </Router>
     </MapProvider>
-
   );
 }
 
