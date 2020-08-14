22ce88dc66c6e9bbcc2cde3a4440595095c72376
imac <chadmdubay@gmail.com>
2020-08-13 21:52:49 -0400

    Updated local storage to load the last searched trip when viewing the map.

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -20,6 +20,7 @@ const useStyles = makeStyles((theme) => ({
       margin: theme.spacing(2, "auto"),
       width: theme.spacing(145),
       height: theme.spacing(70, "auto"),
+      overflow: "auto"
     },
   },
 }));
@@ -39,12 +40,16 @@ const MyFab = styled(Fab)({
 });
 // END OF STYLING
 
+var startingPoint = localStorage.getItem('start');
+var destinationPoint = localStorage.getItem('destination');
+
 export default function Map() {
   const classes = useStyles();
   const { map, setMap } = useContext(MapContext);
 
   const saveTrip = () => {
     const address = map.directionsControl.directions.directionsRequest;
+
     if (address === undefined) {
       toast.error("You should enter at least two states with cities !");
     } else {
@@ -56,50 +61,103 @@ export default function Map() {
       const destinationCity = address.locations[1].adminArea5;
       const destinationState = address.locations[1].adminArea3;
       const destinationPostalCode = address.locations[1].postalCode;
+      const queryOne = `${startCity},+${startState}`;
+      const queryTwo = `${destinationCity},+${destinationState}`;
+      console.log(queryOne);
 
-      const savedTrip = {
-        startCity: startCity,
-        destinationCity: destinationCity,
-        destinationState: destinationState,
-        startState: startState,
-        startStreet: startStreet,
-        destinationStreet: destinationStreet,
-        startPostalCode: startPostalCode,
-        destinationPostalCode: destinationPostalCode,
-      };
-      // if(startCity==undefined||startState==undefined||startStreet==undefined){
-      //   toast.error ("You should enter at least two states with cities !");
+      API.getDirection(queryOne, queryTwo)
+        .then((response) => {
+          console.log(
+            "distance  " + JSON.stringify(response.data.route.distance)
+          );
+          console.log(
+            "time  " + JSON.stringify(response.data.route.formattedTime)
+          );
+          const distance = response.data.route.distance;
+          const time = response.data.route.formattedTime;
+          console.log(distance, time);
 
-      // }else{
-      //   API.saveTrip(savedTrip)
-      //   .then((res) => {
-      //     console.log(res);
-      //     toast.success("You trip is successfully saved !");
-      //     setTimeout(() => window.location.replace('/PastTrips'), 2000);
+          const savedTrip = {
+            time: time,
+            distance: distance,
+            startCity: startCity,
+            destinationCity: destinationCity,
+            destinationState: destinationState,
+            startState: startState,
+            startStreet: startStreet,
+            destinationStreet: destinationStreet,
+            startPostalCode: startPostalCode,
+            destinationPostalCode: destinationPostalCode,
+          };
 
-      //   })
-      //   .catch((err) => {
-      //     console.log("this is error message  " + err);
-      //     toast.error ("You should enter at least two states with cities !");
-
-      //   });
-      // }
-
-      API.saveTrip(savedTrip)
-        .then((res) => {
-          toast.success("You trip is successfully saved !");
-          setTimeout(() => window.location.replace("/PastTrips"), 2000);
+          API.saveTrip(savedTrip)
+            .then((res) => {
+              toast.success("You trip is successfully saved !");
+              setTimeout(() => window.location.replace("/PastTrips"), 2000);
+            })
+            .catch((err) => {
+              console.log("this is error message  " + err);
+              toast.error("Sorry, error occurred! Try once more!");
+            });
         })
         .catch((err) => {
           console.log("this is error message  " + err);
-          toast.error("You should enter at least two states with cities !");
+          toast.error("Sorry, error occurred! Try once more!");
         });
     }
   };
+// DO NOT DELETE THIS CODE
+// ==========================================================================================
+  // const saveTrip = () => {
+  //   const address = map.directionsControl.directions.directionsRequest;
+  //   if (address === undefined) {
+  //     toast.error("You should enter at least two states with cities !");
+  //   } else {
+  //     const startStreet = address.locations[0].street;
+  //     const startCity = address.locations[0].adminArea5;
+  //     const startState = address.locations[0].adminArea3;
+  //     const startPostalCode = address.locations[0].postalCode;
+  //     const destinationStreet = address.locations[1].street;
+  //     const destinationCity = address.locations[1].adminArea5;
+  //     const destinationState = address.locations[1].adminArea3;
+  //     const destinationPostalCode = address.locations[1].postalCode;
+
+  //     const savedTrip = {
+  //       startCity: startCity,
+  //       destinationCity: destinationCity,
+  //       destinationState: destinationState,
+  //       startState: startState,
+  //       startStreet: startStreet,
+  //       destinationStreet: destinationStreet,
+  //       startPostalCode: startPostalCode,
+  //       destinationPostalCode: destinationPostalCode,
+  //     };
+
+  //     API.saveTrip(savedTrip)
+  //       .then((res) => {
+  //         toast.success("You trip is successfully saved !");
+  //         setTimeout(() => window.location.replace("/PastTrips"), 2000);
+  //       })
+  //       .catch((err) => {
+  //         console.log("this is error message  " + err);
+  //         toast.error("Sorry, error occurred! Try once more!");
+  //       });
+  //   }
+  // };
+  // ==========================================================================================
+
 
   useEffect(() => {
+
+    document.addEventListener('keyup',(x)=>{
+        if(document.getElementsByClassName("form-wrap")[0].children[0].children[0].value){
+            localStorage.setItem('start', document.getElementsByClassName("form-wrap")[0].children[0].children[0].value);
+            localStorage.setItem('destination', document.getElementsByClassName("form-wrap")[1].children[0].children[0].value);
+        }
+    });
+
     const mapquest = window.L.mapquest;
-    mapquest.key = "TzrDot8zE5IyvIXUg7RP0ZiSWDnzqxCZ";
+    mapquest.key = process.env.REACT_APP_API_KEY;
     var baseLayer = window.L.mapquest.tileLayer("map");
     var map = window.L.mapquest.map("map", {
       center: [33.753746, -84.38633],
@@ -128,6 +186,15 @@ export default function Map() {
         },
       })
       .addTo(map);
+
+      if(startingPoint&&destinationPoint){
+          window.L.mapquest.directions().route({
+            start: startingPoint,
+            end: destinationPoint
+          });
+          document.getElementsByClassName("form-wrap")[0].children[0].children[0].value = startingPoint;
+          document.getElementsByClassName("form-wrap")[1].children[0].children[0].value = destinationPoint;
+      }
 
     mapquest.geocodingControl().addTo(map);
 
