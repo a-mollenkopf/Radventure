915c1a46647cd78532d1588af12c6ce435d05376
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2020-08-17 14:07:26 -0400

    user is redirected to search page from sign up

--- client/src/components/Signup/Signup.jsx
+++ client/src/components/Signup/Signup.jsx
@@ -32,17 +32,20 @@ const styles = {
 const Signup = () => {
   return (
     <Container maxWidth="sm">
-      <Card style={styles.Card} action="/Signup" method="post">
+      <Card style={styles.Card}>
         <CardContent style={styles.CardContent}>
+        <form  action="/signup" method="post" >
+
           <Typography>Email:</Typography>
           <TextField type="text" name="username" />
           <Typography>Password:</Typography>
           <TextField type="password" name="password" />
           <Typography>Confirm Password:</Typography>
-          <TextField type="text" name="confirmPassword" />
-          <Button className="Signup" size="large" style={styles.ButtonsStyle}>
+          <TextField type="password" name="confirmPassword" />
+          <Button className="Signup" type="submit" value="signup" size="large" style={styles.ButtonsStyle}>
             Sign Up
           </Button>
+          </form>
         </CardContent>
       </Card>
     </Container>
--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 //Passport
-const passport = require('passport')
-, LocalStrategy = require('passport-local').Strategy;
+const passport = require("passport"),
+  LocalStrategy = require("passport-local").Strategy;
 const session = require("express-session");
-const flash = require('connect-flash');
+const flash = require("connect-flash");
 const db = require("./models");
 ////Passport
 
@@ -15,69 +15,79 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-const routes= require ("./routes")
+const routes = require("./routes");
 
 app.use(express.static("client/build"));
 
 //Passport
-app.use(session({
-    cookie: {maxAge:40000},
-    secret: 'something',
+app.use(
+  session({
+    cookie: { maxAge: 40000 },
+    secret: "something",
     resave: false,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+  })
+);
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.use(new LocalStrategy({
- passReqToCallback : true
-}, function(req, usernameInput, passwordInput, done) {
-    db.User.findOne({ username: usernameInput }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validatePassword(passwordInput)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
-  }
-));
-
-app.post('/login',
-  passport.authenticate('local',
-    { successRedirect: '/search',
-      failureRedirect: '/login',
-      failureFlash: 'Invalid username or password.'
-    })
+passport.use(
+  new LocalStrategy(
+    {
+      passReqToCallback: true,
+    },
+    function (req, usernameInput, passwordInput, done) {
+      db.User.findOne({ username: usernameInput }, function (err, user) {
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false, { message: "Incorrect username." });
+        }
+        if (!user.validatePassword(passwordInput)) {
+          return done(null, false, { message: "Incorrect password." });
+        }
+        return done(null, user);
+      });
+    }
+  )
 );
 
-app.post('/signup',(req, res) =>{
-      // const newUser = new db();
-      db.User.create(req.body)
-      .then((dbUsers) => res.json(dbUsers))
-      .catch((err) => console.log(err));
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/search",
+    failureRedirect: "/login",
+    failureFlash: "Invalid username or password.",
+  })
+);
+
+app.post("/signup", (req, res) => {
+  // const newUser = new db();
+  db.User.create(req.body)
+    .then((dbUsers) => {
+        req.login(dbUsers, function(err) {
+          if (err) {
+            console.log(err);
+          }
+          return res.redirect('/search');
+        });
+          })
+    .catch((err) => console.log(err));
 });
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
   done(null, user.id);
 });
 
-passport.deserializeUser(function(id, done) {
-  db.User.findById(id, function(err, user) {
+passport.deserializeUser(function (id, done) {
+  db.User.findById(id, function (err, user) {
     done(err, user);
   });
 });
-//Passport
 
-// app.get("/api/config", (req, res) => {
-//   res.json({
-//     success: true,
-//   });
-// });
-app.use(routes)
+app.use(routes);
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
