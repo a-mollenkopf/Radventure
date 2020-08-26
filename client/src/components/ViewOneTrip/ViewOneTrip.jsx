9571d0793c158b1e0fe80230db440844072b6322
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2020-08-26 18:50:39 -0400

    updates successfuly

--- client/src/components/ViewOneTrip/ViewOneTrip.jsx
+++ client/src/components/ViewOneTrip/ViewOneTrip.jsx
@@ -45,6 +45,7 @@ const styles = {
 
 const ViewOneTrip = () => {
   const [oneTripState, setOneTripState] = useState([]);
+  const { notes } = oneTripState;
   const [event, setEvent] = React.useState({
     title: ``,
     description: ``,
@@ -90,7 +91,7 @@ const ViewOneTrip = () => {
       })
       .catch((err) => console.log(err));
   };
- 
+
   // const handleDeleteItem = (tripID, itemToDelete) => {
   //   const newNotesArray = oneTripState.notes.filter((id) => id._id !== itemToDelete);
   //   setOneTripState([...newNotesArray]);
@@ -154,31 +155,14 @@ const ViewOneTrip = () => {
                     <h4>
                       <strong> Estimated Time:</strong> {oneTripState.time}{" "}
                     </h4>
+                    <hr />
                     <div>
                       <ul>
-                      <h5>List of Items Needed for Trip</h5>
+                        <h5>List of Items Needed for Trip</h5>
 
-                        {oneTripState.notes &&
-                          oneTripState.notes.map(function (name, id) { 
-                            return (
-                              <li key={id._id}>
-                                {name.name}{" "}
-                                {/* <span >
-                                <svg
-                                
-                                  className=" deleteItem MuiSvgIcon-root"
-                                  focusable="false"
-                                  width="24"
-                                  height="23"
-                                  viewBox="0 0 24 24"
-                                  aria-hidden="true"
-                                >
-                                  <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12 1.41 1.41L13.41 14l2.12 2.12-1.41 1.41L12 15.41l-2.12 2.12-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4z"></path>
-                                </svg>
-                                </span> */}
-                               
-                              </li>
-                            );
+                        {notes &&
+                          notes.map((name, id) => {
+                            return <li key={id._id}>{name.name} </li>;
                           })}
                       </ul>
                     </div>
